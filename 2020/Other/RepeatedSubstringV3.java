a591f03f32add28f64936260ffe8ff8821790029
Nathnael Alemu <nalemu@luc.edu>
2020-08-25 11:41:15 -0400

    Trie implementation of repeated substring

--- 2020/Other/RepeatedSubstringV3.java
+++ 2020/Other/RepeatedSubstringV3.java
@@ -1,10 +1,12 @@
 
-
+import java.util.*;
+import java.io.*;
 /**
 Algorithm 3:
 	- build an augmented suffix trie(as a ternary trie) with repeitition info in each node. then query the whole trie to count.
 
 **/
+
 
 public class RepeatedSubstringV3 {
 
@@ -12,7 +14,7 @@ public class RepeatedSubstringV3 {
 		TrieNode root;
 
 		class TrieNode {
-			int repeatition;
+			int repetition;
 			char c;
 			TrieNode left, forward, right;
 			public TrieNode(char c) {
@@ -26,15 +28,41 @@ public class RepeatedSubstringV3 {
 			}
 		}
 
-		private void insert(TrieNode node, String str, int curr) {
+		private int insert(TrieNode node, String str, int curr) {
 			//TODO: Make sure to augment with repeatead
+			char currentChar = str.charAt(curr);
+			if (node == null) node = new TrieNode(currentChar);
+
+			// it's important the number of repetitions of the siblings. even though it won't affect the number of repetitions of siblings.
+			if (currentChar > node.c) return node.repetition + insert(node.right, str, curr);
+			else if (currentChar < node.c) return node.repetition + insert(node.left, str, curr);
+			else if (curr == str.length() - 1) {
+				node.repetition = 1;
+				return node.repetition;
+			}
+			else {
+				int numRepeated = insert(node.forward, str, curr + 1)
+				node.repetition += numRepeated;
+				return numRepeated
+			}
 		}
 
 		public int traverse() {
 			return traverse(root);
 		}
 		private int traverse(TrieNode current) {
-			//TODO
+			Queue<TrieNode> siblingIterator = new LinkedList<>();
+
+			siblingIterator.add(current);
+			int numRepeated = 0;
+			while (!siblingIterator.isEmpty()) {
+				TrieNode current = siblingIterator.poll();
+				if (current.repetition > 1) numRepeated += 1;
+				if (current.left != null) siblingIterator.add(current.left);
+				if (current.right != null) siblingIterator.add(current.right);
+				if (current.forward != null) numRepeated += traverse(current.forward);
+			}
+			return numRepeated;
 		}
 	}
 
